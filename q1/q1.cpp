1514e2c94f9c3431b69ac17de585c879f2365cd7
mag0 <giordano.marcelo@gmail.com>
2023-04-19 17:33:34 -0300

    :repeat: refactoring structs

--- q1/q1.cpp
+++ q1/q1.cpp
@@ -2,12 +2,49 @@
 #include <stdlib.h>
 #include "q1.h"
 
+float Comodo::CalculaArea(){
+  area = (w*h);
+  return area; 
+}
+
+float Comodo::Lampada(){
+  float pot;
+  switch(t){
+      case 0:
+        pot = 12;
+        break;
+      case 1:
+        pot = 15;
+        break;
+      case 2:
+        pot = 18;
+        break;
+      case 3:
+        pot = 20;
+        break;
+      case 4:
+        pot = 22;
+        break;
+    }
+  return ceil((pot*area)/60);
+}
+
+Comodo::Comodo(int x, float y, float z):t(x),w(y),h(z){
+  CalculaArea();
+  std::cout << Lampada() << std::endl;
+}
+
 int main(){
   int tc = 0;
   float w{0},h{0};
   do{
     std::cin >> tc;
-    if (tc == -1) return 0;
+    switch(tc){
+      case 0: case 1: case 2: case 3: case 4:
+        break;
+      default:
+        return 0;
+    }
     std::cin >> w >> h;
     Comodo c(tc,w,h);
   } while (true);
--- q1/q1.h
+++ q1/q1.h
@@ -1,38 +1,12 @@
 #pragma once
 #include <math.h>
 
-typedef struct Comodo {
+struct Comodo{
   private:
     int t;
     float w, h, area;
   public:
-    Comodo(int x, float y, float z):t(x),w(y),h(z){
-      CalculaArea();
-      std::cout << Lampada() << std::endl;
-    }
-    float CalculaArea(){
-      area = (w*h);
-      return area; 
-    }
-    float Lampada(){
-      float pot{0};
-      switch(t){
-        case 0:
-          pot = 12;
-          break;
-        case 1:
-          pot = 15;
-          break;
-        case 2:
-          pot = 18;
-          break;
-        case 3:
-          pot = 20;
-          break;
-        case 4:
-          pot = 22;
-          break;
-      }
-      return ceil((pot*area)/60);
-    }
-} Comodo;
+    Comodo(int x, float y, float z);
+    float CalculaArea();    
+    float Lampada();
+};
